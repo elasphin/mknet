b5ad926472b88d685600271bbbad93a6ecba5dbe
elasphin <elasphin@gmail.com>
2026-09-21 17:26:53 +0330

    Update mknet_c_v111_15state_9loss_biasfixed_fde_phase6.py

--- mknet_c_v111_15state_9loss_biasfixed_fde_phase6.py
+++ mknet_c_v111_15state_9loss_biasfixed_fde_phase6.py
@@ -1651,8 +1651,8 @@ if __name__ == '__main__':
         )
     OUTPUT_DIR = Path('/kaggle/working/direct_run')
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-    MAX_FUSION_EPOCHS = 1601
-    MAX_TEST_FUSION_EPOCHS = 300
+    MAX_FUSION_EPOCHS = 500
+    MAX_TEST_FUSION_EPOCHS = 50
     TRAINING_EPOCHS = 20
     LEARNING_RATE = 1e-4
     OPTIMIZER_WINDOW_SIZE = 4
